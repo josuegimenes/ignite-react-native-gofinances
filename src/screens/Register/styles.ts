5c417ba5a1a3d8d60d14990c2c4cc28436909d1b
Josué Gimenes <josuegalvares@gmail.com>
2021-10-24 18:46:18 -0300

    Select Component

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -36,5 +36,8 @@ export const Fields = styled.View``;
 export const TransactionsTypes = styled.View`
   flex-direction: row;
   justify-content: space-between;
+
+  margin-top: 8px;
+  margin-bottom: 16px;
 `;
 
