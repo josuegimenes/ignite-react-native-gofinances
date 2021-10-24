0289a5be335f9978a06d552274a6a5a23363f25a
Josué Gimenes <josuegalvares@gmail.com>
2021-10-24 18:26:51 -0300

    Creating the Component to Select the Transaction Type

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -32,3 +32,9 @@ export const Form = styled.View`
 `;
 
 export const Fields = styled.View``;
+
+export const TransactionsTypes = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
