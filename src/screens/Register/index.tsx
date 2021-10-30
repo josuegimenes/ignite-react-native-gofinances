5d229a08a73b5ce01b81061aa5491fc82df716ac
Josué Gimenes <josuegalvares@gmail.com>
2021-10-30 17:19:06 -0300

    Storing and Retrieving Data

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { 
   Modal, 
   TouchableWithoutFeedback, 
@@ -7,10 +7,10 @@ import {
 } from 'react-native'
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { useForm } from 'react-hook-form'
 
-import { Input } from '../../components/Form/Input'
 import { InputForm } from '../../components/Form/InputForm'
 import { Button } from '../../components/Form/Button'
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton'
@@ -47,6 +47,8 @@ export function Register() {
   const [transactionType, setTransactionType] = useState('')
   const [categoryModalOpen, setCategoryModalOpen] = useState(false)
 
+  const dataKey = '@gofinances:transactions'
+
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria'
@@ -72,7 +74,7 @@ export function Register() {
     setCategoryModalOpen(false)
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
     if(!transactionType) 
       return Alert.alert('Selecione o tipo da transação')
 
@@ -85,8 +87,24 @@ export function Register() {
       transactionType,
       category: category.key
     }
-    console.log(data)
+    
+    try {
+      await AsyncStorage.setItem(dataKey, JSON.stringify(data))
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar")
+    }
   }
+
+  useEffect(() => {
+    async function loadData() {
+      const data = await AsyncStorage.getItem(dataKey)
+      console.log(JSON.parse(data!))
+    }
+
+    loadData()
+  }, [])
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
