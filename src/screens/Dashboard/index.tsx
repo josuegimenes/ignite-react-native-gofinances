7e73e842fbad516f1fe2aaa219c52054b286e575
Josué Gimenes <josuegalvares@gmail.com>
2021-10-30 16:07:11 -0300

    RectButton and BorderlessButton

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -16,7 +16,8 @@ import {
   HighlightCards,
   Transactions,
   Title,
-  TransactionList
+  TransactionList,
+  LogoutButton
 } from './styles'
 
 export interface DataListProps extends TransactionCardProps {
@@ -73,8 +74,10 @@ export function Dashboard() {
               <UserName>Josué</UserName>
             </User>
           </UserInfo>
-
-          <Icon name="power" />
+          
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
         </UserWrapper>
       </Header>
       
